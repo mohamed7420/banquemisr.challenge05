2b2718d91ada3ff1a2a3884a55b7db260e64e4a0
Mohamed Osama <mabdulmajid.c@nwc.com.sa>
2024-05-05 21:06:06 +0300

    creating configuration manager to get value from xconfig file

---
+++ banquemisr.challenge05/Services/Configuration/ConfigurationManager.swift
@@ -0,0 +1,46 @@
+//
+//  ConfigurationManager.swift
+//  banquemisr.challenge05
+//
+//  Created by Mohamed Osama on 05/05/2024.
+//
+
+import Foundation
+
+struct ConfigurationManager {
+    
+    enum ConfigurationError: Error {
+        case invalidPath
+        case invalidInfo
+    }
+    
+    enum Key: String {
+        case apiKey = "APIKey"
+        case imageURL = "imageURL"
+        case accessToken = "accessToken"
+    }
+    
+    static let shared = ConfigurationManager()
+    
+    private init() { }
+    
+    func xconfigValue(key: Key) throws -> String {
+        guard let configURL = Bundle.main.url(forResource: "AppConfiguration", withExtension: "xcconfig") else {
+            throw ConfigurationError.invalidPath
+        }
+        
+        let configString = try String(contentsOf: configURL)
+        guard let apiKeyLine = configString.components(separatedBy: .newlines)
+            .first(where: { $0.contains(key.rawValue) }) else {
+            throw ConfigurationError.invalidInfo
+        }
+        
+        let parts = apiKeyLine.components(separatedBy: "=")
+        guard parts.count == 2 else {
+            throw ConfigurationError.invalidInfo
+        }
+        
+        let apiKey = parts[1].trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "\"", with: "")
+        return apiKey
+    }
+}
