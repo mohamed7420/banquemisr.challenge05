c0a5688b23cead207a529921c5fabe04b2187b64
Mohamed Osama <mabdulmajid.c@nwc.com.sa>
2024-05-05 20:18:36 +0300

    Initial Commit

---
+++ banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by Mohamed Osama on 05/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
