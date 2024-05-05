a0a48eb947e0e231aec2f2f36706d45be798a3bc
Mohamed Osama <mabdulmajid.c@nwc.com.sa>
2024-05-05 20:37:51 +0300

    setup shimmer and set it's constrains

--- banquemisr.challenge05/Scene/Now Playing/View/Shimmer/MovieShimmerView.swift
+++ banquemisr.challenge05/Scene/Now Playing/View/Shimmer/MovieShimmerView.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class MovieShimmerView: UIView {
+    lazy var shimmeringView: FBShimmeringView = {
+        let view = FBShimmeringView(frame: self.bounds)
+        view.isShimmering = true
+        view.shimmeringSpeed = 300
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addSubview(shimmeringView)
+        shimmeringView.contentView = subviews.first
+        
+        NSLayoutConstraint.activate([
+            shimmeringView.topAnchor.constraint(equalTo: topAnchor),
+            shimmeringView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            shimmeringView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            shimmeringView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
