b8437179eaeaa4982fd2e40ab88c7fc0f9a0a145
Mohamed Osama <mabdulmajid.c@nwc.com.sa>
2024-05-05 20:46:46 +0300

    creating load all popular movies request and handling select movie

--- banquemisr.challenge05/Scene/Popular/ViewModel/PopularViewModel.swift
+++ banquemisr.challenge05/Scene/Popular/ViewModel/PopularViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class PopularViewModel {
+    public var manager = NetworkService()
+    private let storageManager = StorageManager.shared
+
+    @Published var isLoading = true
+    @Published var movies: [Movie] = []
+    
+    @discardableResult
+    func loadPopularMovies(page: Int = 1) async throws -> PopularMovieResponse {
+        isLoading = true
+        defer { isLoading = false }
+        
+        let response: PopularMovieResponse = try await manager.request(path: .popular, parameters: ["page": "\(page)"])
+        movies = response.results
+        storageManager.saveMovies(movies: movies)
+        return response
+    }
+    
+    func selectedMovie(at index: Int) -> Movie? {
+        guard !movies.isEmpty else { return nil }
+        
+        return movies[index]
+    }
+}
